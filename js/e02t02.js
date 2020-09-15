471b67be559465ad512cde66808f217322a199ed
Karel Haapasalo <karel.haapasalo@hotmail.com>
2020-09-15 15:39:52 +0300

    made containsNumbers function

--- js/e02t02.js
+++ js/e02t02.js
@@ -1,3 +1,48 @@
+// function 1
+const containsNumbers = function(numbers, aNumber) {
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] === aNumber) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// function 2
+var containsNumberIO = function(numbers, aNumber) {
+    if (numbers.indexOf(aNumber) >= 0) {
+        return true;
+    }
+
+    return false;
+}
+
+// function 3
+const containsNumberI = function(numbers, aNumber) {
+    if (numbers.includes(aNumber)) {
+        return true;
+    }
+
+    return false;
+}
+
+function displayResult() {
+    let aNumber = 7;
+    let testNumbers = [1,4,2,5,6,7,5,7,2];
+    let message;
+
+    if (containsNumberI(testNumbers, aNumber)) {
+        message = "Array contains the number " + aNumber;
+    } else {
+        message = "Array doesnt contain the number " + aNumber;
+    }
+
+    document.getElementById("content").innerHTML = message;
+}
+
+window.onload = displayResult;
+
+/*
 let elements = [1, 4, 2, 5, 6, 7, 5, 7, 2];
 let aNumber = 6;
 
@@ -13,4 +58,4 @@ let containsNumbers = function(numbers, aNumber) {
 }
 
 console.log(containsNumbers(elements, aNumber));
-
+*/

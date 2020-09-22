4c9446a4d0f6eab434bf2d6b4f60bf23f7383f17
Karel Haapasalo <karel.haapasalo@hotmail.com>
2020-09-22 11:30:33 +0300

    made isLeapYear program to work as intended

--- js/e02t01.js
+++ js/e02t01.js
@@ -1,12 +1,16 @@
+function displayResult() {
+    let year = document.getElementById("inputYear").value;
+    if (isLeapYear(year)) {
+        document.getElementById("content").innerHTML = "Year " + 
+        year + " is a leap year";
+    } else if (isLeapYear(year) == false) {
+        document.getElementById("content").innerHTML = "Year " + 
+        year + " is not a leap year" ;
+    } else console.log("something went wrong");
+}
+
 function isLeapYear(year) {
     if (year % 400 === 0) return true;
     if (year % 100 === 0) return false;
     return year % 4 === 0;
 }
-
-function displayResult() {
-    let year = 2019;
-    document.getElementById("content").innerHTML = isLeapYear(year);
-}
-
-window.onload = displayResult;
